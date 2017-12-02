84bc71701a2e171a047fa73e272e0d4258ca10a4
Shame625 <sarickk@gmail.com>
2017-12-02 15:47:10 +0100

    Uplaod to git

---
+++ BCNF/Class1.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BCNF
+{
+
+    class Atribut
+    {
+        public string content { get; set; }
+
+        public Atribut(string s)
+        {
+            content = s;
+        }
+
+        public override string ToString()
+        {
+            return content;
+        }
+    }
+
+    class Skup_Atributa
+    {
+        public Skup_Atributa(Skup_Atributa other) // "Deep copy" konstruktor
+        {
+            skup = new List<Atribut>();
+            foreach (Atribut a in other.skup)
+            {
+                skup.Add(a) ;
+            }
+        }
+        public Skup_Atributa()
+        {
+            skup = new List<Atribut>();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", skup);
+        }
+
+        public string MyStr()
+        {
+            return string.Join("", skup);
+        }
+
+        public List<Atribut> skup { get; set; }
+
+        public bool Contains(Atribut atribut_za_provjeru) //Novi Contains
+        {   
+            int flag = 0;
+            foreach (Atribut a in skup)
+            {
+                if (a.content == atribut_za_provjeru.content)
+                {
+                    flag = 1;
+                }
+            }
+            if (flag!=0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class FO
+    {
+        public FO()
+        {
+            Lijevo = new Skup_Atributa();
+            Desno = new Skup_Atributa();
+        }
+        public Skup_Atributa Lijevo { get; set; }
+        public Skup_Atributa Desno { get; set; }
+
+        public override string ToString()
+        {
+            string l = string.Join(", ", Lijevo.skup);
+            string d = string.Join(", ", Desno.skup);
+            return '{' + l + " -> " + d + '}';
+        }
+
+        public string MyStr()
+        {
+            string l = string.Join("|", Lijevo.skup);
+            string d = string.Join("|", Desno.skup);
+            return l + "->" + d;
+        }
+    }
+
+    class Problem
+    {
+        public Problem()
+        {
+            veliko_R = new Skup_Atributa();
+            relacije = new List<FO>();
+            kljucevi = new List<Skup_Atributa>();
+        }
+
+        public Skup_Atributa veliko_R { get; set; }
+        public List<Skup_Atributa> kljucevi { get; set; }
+        public List<FO> relacije { get; set; }
+        MainWindow myWin = ((MainWindow)System.Windows.Application.Current.MainWindow);
+
+        public bool Contains(Skup_Atributa potencijalni_kljuc)
+        {
+            foreach (Skup_Atributa sa in kljucevi)
+            {
+                int brojac = 0;
+                foreach (Atribut a in sa.skup)
+                {
+                    foreach (Atribut a_pk in potencijalni_kljuc.skup)
+                    {
+                        if (a.content == a_pk.content)
+                        {
+                            brojac++;
+                        }
+                    }
+                }
+                if (brojac == sa.skup.Count())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Pronadji_Kljuceve()
+        {
+            myWin.debugBox.Text = "";
+            kljucevi.Clear();
+            pronadi_kljuceve_rekurzija(veliko_R);
+        }
+
+        bool pronadi_kljuceve_rekurzija(Skup_Atributa potencijalni_kljuc)
+        { 
+            int flag = 0;
+            //gleda jeli ograda kljuca pokriva R
+            if (!provjeri_kljuc(potencijalni_kljuc))
+            {
+                return false;
+            }
+            List<Skup_Atributa> dijelovi_kljuca = new List<Skup_Atributa>();
+            int velicina = potencijalni_kljuc.skup.Count;
+            //razbija kljuc u podkljuceve
+            for (int i = 0; i < velicina; i++)
+            {
+                Skup_Atributa temp = new Skup_Atributa();
+                for (int j = 0; j < velicina - 1; j++)
+                {
+                    int offset = (i + j) % velicina;
+                    temp.skup.Add(new Atribut(potencijalni_kljuc.skup[offset].content));
+                }
+                temp.skup = temp.skup.OrderBy(o => o.content).ToList();
+                dijelovi_kljuca.Add(temp);
+            }
+            foreach (Skup_Atributa sa in dijelovi_kljuca)
+            {
+                //provjerava za dijelove kljuca jesu li vec kljc, ako jesu stavlja flag na 1
+                if (Contains(sa))
+                {
+                    flag = 1;
+                }
+                //provjerava rekurzivno za dijelove kljuca jeli pokrivaju R, ako jesu stavlja flag na 1
+                else if (pronadi_kljuceve_rekurzija(sa))
+                {
+                    flag = 1;
+                }
+            }
+            //ako je flag 1 vraca true sto znaci da ovaj kljuc nije minimalan niti njegovi nadkljucevi ili vec postoji
+            if (flag == 1)
+            {
+                return true;
+            }
+
+            kljucevi.Add(potencijalni_kljuc);
+            dijelovi_kljuca.Clear();
+
+            return true;
+        }
+         
+        bool provjeri_kljuc(Skup_Atributa potencijalni_kljuc)
+        {
+            myWin.Append("Ograda: "+potencijalni_kljuc.ToString());
+
+            Skup_Atributa temp = new Skup_Atributa(potencijalni_kljuc);//CP konstruktor(RADI!!)
+            int brojac = 0;
+            int size = potencijalni_kljuc.skup.Count();
+            while (size >= 0)
+            { 
+                foreach (FO fo in relacije)
+                {
+                    foreach (Atribut at_left in fo.Lijevo.skup)
+                    {
+                        foreach (Atribut atributi_kljuca in temp.skup)
+                        {
+                            if (at_left.content == atributi_kljuca.content)
+                                brojac++;
+                
+                        }
+                    }
+                    
+                    if (brojac == fo.Lijevo.skup.Count())
+                    {
+                        foreach (Atribut at_right in fo.Desno.skup)
+                        {   
+                            Atribut novi = new Atribut(at_right.content);
+                            
+                            if (!temp.Contains(novi))
+                            {
+                                temp.skup.Add(novi);
+                            }
+                        }
+                    }
+                    brojac = 0;
+                }
+                size--;
+            }
+
+            if (temp.skup.Count() >= veliko_R.skup.Count())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ BCNF/MainWindow.xaml.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BCNF
+{
+    public partial class MainWindow : Window
+    {
+        private static string RemoveWhitespace(string input)
+        {
+            return new string(input.ToCharArray()
+                .Where(c => !Char.IsWhiteSpace(c))
+                .ToArray());
+        }
+
+        Problem problem;
+        bool debugMode = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            problem = new Problem();
+        }
+
+        private void unesiButton_Click(object sender, RoutedEventArgs e)
+        {
+            UnosFO(foBox.Text);
+        }
+
+        private void UnosAtributa(string str)
+        {
+            problem.veliko_R.skup.Clear();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                MessageBox.Show("Nepostoje atributi!", "Prazno!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string[] atributi = str.Split(',');
+            atributi = atributi.Select(s => RemoveWhitespace(s.ToUpper())).ToArray();
+            atributi = atributi.Distinct().ToArray();
+            atributi = atributi.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+            foreach (string s in atributi)
+            {
+                Atribut novi = new Atribut(s);
+
+                problem.veliko_R.skup.Add(novi);
+            }
+
+            UpdateAtributaListBox();
+        }
+
+        private void UnosFO(string st)
+        {
+            problem.relacije.Clear();
+            if (string.IsNullOrWhiteSpace(st))
+            {
+                MessageBox.Show("Nepostoje relacije!", "Prazno!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string[] relacije = st.Split(',');
+            relacije = relacije.Select(s => RemoveWhitespace(s.ToUpper())).ToArray();
+            relacije = relacije.Distinct().ToArray();
+            relacije = relacije.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+            foreach (string str in relacije)
+            {
+                string[] Strane = str.Split(new string[] { "->" }, StringSplitOptions.None);
+                if(Strane.Count() == 2)
+                {
+                    string[] Lijevo = Strane[0].Split('|');
+                    string[] Desno = Strane[1].Split('|');
+
+                    FO nova = new FO();
+
+                    foreach(string s in Lijevo)
+                    {
+                        nova.Lijevo.skup.Add(new Atribut(s));
+                    }
+                    foreach (string s in Desno)
+                    {
+                        nova.Desno.skup.Add(new Atribut(s));
+                    }
+                    problem.relacije.Add(nova);
+                }
+               
+            }
+            UpdateRelacijeListBox();
+        }
+
+        private void listBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                int prev = 0;
+                if (relacije_listBox.SelectedIndex >= 0)
+                {
+                    prev = relacije_listBox.SelectedIndex;
+                    problem.relacije.RemoveAt(prev);
+                    UpdateRelacijeListBox();
+
+                    relacije_listBox.SelectedIndex = prev;
+                    relacije_listBox.Focus();
+                }
+            }
+        }
+
+        public void Append(string str)
+        {
+            if(debugMode)
+                debugBox.Text += str + '\n';
+        }
+
+        private void UpdateRelacijeListBox()
+        {
+            relacije_listBox.ItemsSource = null;
+            relacije_listBox.ItemsSource = problem.relacije;
+        }
+
+        private void UpdateAtributaListBox()
+        {
+            atributi_listBox.ItemsSource = null;
+            atributi_listBox.ItemsSource = problem.veliko_R.skup;
+        }
+
+        private void UpdateKljuceviListBox()
+        {
+            kljucevi_listBox.Items.Clear();
+
+            for (int i= problem.kljucevi.Count()-1; i>=0;i--)
+            {
+                string t = "";
+                foreach (Atribut s in problem.kljucevi[i].skup)
+                {
+                    t += s.ToString();
+                }
+                kljucevi_listBox.Items.Add(t);
+            }
+        }
+
+        private void foBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                UnosFO(foBox.Text);
+            }
+        }
+
+        private void unesiAtributeBottun_Click(object sender, RoutedEventArgs e)
+        {
+            UnosAtributa(atributiBox.Text);
+        }
+
+
+        private void atributiBox_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                UnosAtributa(atributiBox.Text);
+        }
+
+        private void atributi_listBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                int prev = 0;
+                if (atributi_listBox.SelectedIndex >= 0)
+                {
+                    prev = atributi_listBox.SelectedIndex;
+                    problem.veliko_R.skup.RemoveAt(prev);
+                    UpdateAtributaListBox();
+
+                    atributi_listBox.SelectedIndex = prev;
+                    atributi_listBox.Focus();
+                }
+            }
+        }
+
+        private void kljuceviButton_Click(object sender, RoutedEventArgs e)
+        {
+            problem.Pronadji_Kljuceve();
+            UpdateKljuceviListBox();
+        }
+
+
+        string filepath = "";
+
+        void LoadFile()
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text|*.txt|All|*.*";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                filepath = dlg.FileName;
+                loadFromFile.Text = filepath;
+            }
+        }
+
+        private void loadFromFile_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(filepath))
+            {
+                MessageBox.Show("Odaberite lokaciju fajla prvo!", "Odabir!");
+                LoadFile();
+                return;
+            }
+            if (File.Exists(filepath))
+            {
+            string readText = File.ReadAllText(filepath);
+            readText = readText.Replace("\n", String.Empty).Replace("\t", String.Empty).Replace("\r", String.Empty);
+
+            string[] temp = readText.Split(';');
+
+                if (temp.Length >= 3)
+                {
+                    UnosAtributa(temp[0]);
+                    UnosFO(temp[1]);
+
+                    if (temp[2].Length > 0)
+                    {
+                        problem.Pronadji_Kljuceve();
+                        UpdateKljuceviListBox();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Loš format učitane datoteke!");
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string fileText = "";
+
+            //radimo string
+
+            foreach (Atribut sa in problem.veliko_R.skup)
+            {
+                fileText += sa.ToString() + ',';
+            }
+
+            if (string.IsNullOrEmpty(fileText))
+            {
+                MessageBox.Show("Greška pri zapisivanju atributa!", "Greška");
+                return;
+            }
+
+            fileText = fileText.Remove(fileText.Length - 1, 1) + ";" + System.Environment.NewLine;
+
+            string FOstr = "";
+
+            foreach (FO fo in problem.relacije)
+            {
+                FOstr += fo.MyStr() + ',';
+            }
+
+            if (string.IsNullOrEmpty(FOstr))
+            {
+                MessageBox.Show("Greška pri zapisivanju relacija!", "Greška");
+                return;
+            }
+
+            FOstr = FOstr.Remove(FOstr.Length - 1, 1) + ";" + System.Environment.NewLine;
+
+
+
+            fileText += FOstr;
+
+            string Keys = "";
+            foreach (Skup_Atributa pk in problem.kljucevi)
+            {
+                Keys += pk.MyStr() + ',';
+            }
+
+            if (string.IsNullOrEmpty(Keys))
+            {
+                MessageBox.Show("Greška pri zapisivanju ključeva!", "Greška");
+                return;
+            }
+
+            Keys = Keys.Remove(Keys.Length - 1, 1) + ";";
+
+
+
+            fileText += Keys;
+
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, fileText);
+            }
+        }
+
+        private void debugCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            debugMode = debugCheckBox.IsChecked.Value;
+
+            debugCanvas.IsEnabled = true;
+            BrushConverter bc = new BrushConverter();
+            Brush brush;
+
+            if (debugMode)
+            {
+                debugCanvas.IsEnabled = true;
+                brush = (Brush)bc.ConvertFrom("#FF42191D");
+                brush.Freeze();
+                debugCanvas.Background = brush;
+            }
+            else
+            {
+                debugCanvas.IsEnabled = false;
+                brush = (Brush)bc.ConvertFrom("#FFA29C9D");
+                brush.Freeze();
+                debugCanvas.Background = brush;
+            }
+        }
+    }
+}
